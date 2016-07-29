69d2c9015a0f50019d77603ff8feee451f78a908
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2016-07-29 14:48:23 -0700

    Updating

---
+++ src/test/java/jetty/uber/EnvTest.java
@@ -0,0 +1,25 @@
+package jetty.uber;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class EnvTest
+{
+    @Test
+    public void testEnv()
+    {
+        System.out.println("$HOME = " + System.getenv("HOME"));
+        System.out.println("$M2_HOME = " + System.getenv("M2_HOME"));
+        System.out.println("$MAVEN_HOME = " + System.getenv("MAVEN_HOME"));
+        System.out.println("$JAVA_HOME = " + System.getenv("JAVA_HOME"));
+    
+        System.getProperties().entrySet().stream()
+                .filter(entry -> entry.getKey().toString().matches("^(java|sun|user)\\..*"))
+                .sorted((e1, e2) -> e1.getKey().toString().compareTo(e2.getKey().toString()))
+                .forEach(entry -> System.out.println("  " + entry.getKey() + " = " + entry.getValue()));
+        
+        assertThat("BRANCH Exists", System.getenv("BRANCH"), notNullValue());
+    }
+}
